e94833b2d5399f7f4e2f4685fc20b179877a807f
w3bdesign <hijack_xxx@yahoo.com>
2020-01-23 22:10:33 +0100

    Testing Scrollmagic

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,18 @@
+// init controller
+var controller = new ScrollMagic.Controller();
+// build scene
+new ScrollMagic.Scene({
+    triggerElement: "#reveal1",
+    triggerHook: 0.9, // show, when scrolled 10% into view
+    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal1", "visible") // add class to reveal
+//.addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+
+
 const startAnimation = () => {
   // Lag en timeline instans
   const timeline = gsap.timeline();
