d0c67aa3c5def9bace12358d1912b75001de9734
w3bdesign <hijack_xxx@yahoo.com>
2020-01-23 20:50:05 +0100

    .

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,16 @@
 const startAnimation = () => {
-// Lag en timeline instans
-const timeline = gsap.timeline();
+  // Lag en timeline instans
+  const timeline = gsap.timeline();
 
-// Main nav
-//timeline.from('.main-nav', { duration: 0.5 });
-timeline.from('.main-nav-hjem', { duration: 0.4, x: -2500 });
-timeline.from('.main-nav-ommeg', { duration: 0.4, x: -2500 });
-timeline.from('.main-nav-cv', { duration: 0.4, x: -2500 });
-timeline.from('.main-nav-kontakt', { duration: 0.4, x: -2500 });
+  // Main nav
+  //timeline.from('.main-nav', { duration: 0.5 });
+  timeline.from('.main-nav-hjem', { duration: 0.4, x: -2500 });
+  timeline.from('.main-nav-ommeg', { duration: 0.4, x: -2500 });
+  timeline.from('.main-nav-cv', { duration: 0.4, x: -2500 });
+  timeline.from('.main-nav-kontakt', { duration: 0.4, x: -2500 });
 
-// Bokser
-/*
+  // Bokser
+  /*
 timeline.from('.showcase', { duration: 0.4, x: 2500 });
 timeline.from('.top-box-a', { duration: 0.4, x: 2500 });
 timeline.from('.bokser .boks', { duration: 0.4, opacity: 0 });
@@ -28,11 +28,8 @@ timeline.from('.port-5', { duration: 0.4, opacity: 0 });
 timeline.from('.port-6', { duration: 0.4, opacity: 0 });
 */
 
-// Footer
-//timeline.from('footer', { duration: 1, opacity: 0 });
-}
+  // Footer
+  //timeline.from('footer', { duration: 1, opacity: 0 });
+};
 
 startAnimation();
-
-
-
