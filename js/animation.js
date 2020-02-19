f9dd4999aab80759e6ee272cf6cd5baf288111d9
w3bdesign <hijack_xxx@yahoo.com>
2020-02-19 04:52:24 +0100

    Replace GSAP
    Replace GSAP with CSS animations

--- js/animation.js
+++ js/animation.js
@@ -14,23 +14,4 @@ const startScrollMagic = () => {
   }
 };
 
-const startAnimation = () => {
-  // Lag en timeline instans
-  const timeline = gsap.timeline();
-  
-  timeline.to('.main-nav', { duration: 0.5, opacity: 1 });  
-
-  timeline.from('.topp-kontainer', { duration: 0.5, opacity: 0 });  
-  timeline.from('.portfolio-header', { duration: 0.5, opacity: 0 }); 
-  timeline.from('.portfolio', { duration: 0.5, opacity: 0 }); 
- 
-
-  // Main nav
-  timeline.from('.main-nav-hjem', { duration: 0.4, x: -2500 });
-  timeline.from('.main-nav-ommeg', { duration: 0.4, x: -2500 });
-  timeline.from('.main-nav-cv', { duration: 0.4, x: -2500 });
-  timeline.from('.main-nav-kontakt', { duration: 0.4, x: -2500 });
-};
-
-startAnimation();
 startScrollMagic();
