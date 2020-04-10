c1734009fc7d5b6b5e99e566e7379c45dc6c667e
w3bdesign <hijack_xxx@yahoo.com>
2020-04-10 04:25:39 +0200

    Fix for PurgeCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,17 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-postcss",
-    "gatsby-plugin-purgecss",
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: false, // Print removed selectors and processed file names
+         develop: true, // Enable while using `gatsby develop`
+         tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
