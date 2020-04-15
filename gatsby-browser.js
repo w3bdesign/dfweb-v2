9c2f101374b0f7b17287d222680d3514252a3439
w3bdesign <hijack_xxx@yahoo.com>
2020-04-15 03:05:31 +0200

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,6 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./src/css/main.css"
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+// You should do that in a Layout file or in `gatsby-browser.js`.
+config.autoAddCss = false;
