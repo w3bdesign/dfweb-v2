95fe6506ad96aae1a9e2da025c5c0b8a4dc0cf42
w3bdesign <hijack_xxx@yahoo.com>
2020-04-16 03:41:19 +0200

    Update Portfolio.component.jsx

--- src/components/Animation/Portfolio.component.jsx
+++ src/components/Animation/Portfolio.component.jsx
@@ -1,19 +1,42 @@
 import * as React from "react"
 //import { motion } from "framer-motion"
 
-import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion"
 
+/*
 const container = {
   hidden: { opacity: 0, scale: 0 },
   visible: {
     opacity: 1,
     scale: 1,
     transition: {
-      delay: 1.0,
+      delay: 0.5,
       when: "beforeChildren",
-      staggerChildren: 1.0,
+      staggerChildren: 0.5,
     },
   },
+}
+*/
+
+const container = {
+  hidden: { opacity: 0, scale: 0, y:"100%" },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    y:"0%",
+    transition: {
+      delay: 1,
+      staggerChildren: 1,
+      ease: "easeInOut",
+     
+    },
+  },
+}
+
+const transition = {
+  duration: 1,
+  ease: "easeInOut",
+  staggerChildren: 1,
 }
 
 export const Portfolio = () => (
@@ -23,12 +46,12 @@ export const Portfolio = () => (
         key={index}
         className="p-12 text-lg text-white rounded bg-hero"
         variants={container}
+        transition={{duration: index}}
         initial="hidden"
         animate="visible"
       >
-        <p className="m-2 text-lg">Innhold Portefølje</p>
-        <motion.div>Les mer innhold</motion.div>
-        
+        <p className="text-lg text-center">Innhold Portefølje</p>
+        <motion.div variants={container} initial="hidden" animate="visible" className="text-lg text-center">Les mer innhold</motion.div>
       </motion.div>
     ))}
   </>
