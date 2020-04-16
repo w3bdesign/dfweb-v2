d4ce2280e6b97c3f9f61692becb9cc2e67b557b4
w3bdesign <hijack_xxx@yahoo.com>
2020-04-16 03:14:26 +0200

    Add sample headers to portfolio

--- src/components/Portefølje/MainContent.js
+++ src/components/Portefølje/MainContent.js
@@ -6,15 +6,26 @@ function MainContent() {
   return (
     <>
       <div className="container mx-auto rounded">
-        <div className="container p-4 mx-auto mt-4 ">
-          <div className="p-12 font-sans text-lg text-white rounded bg-hero">
-            Test
+        <div className="p-4 mx-auto mt-4 ">
+          <div className="p-4 font-sans text-lg font-bold text-center text-white rounded bg-hero">
+            Javascript
           </div>
         </div>
         <div className="grid gap-4 p-4 mt-4 lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1 ">
           <Portfolio />
+        </div>
+        <div className="p-4 mx-auto mt-4 ">
+          <div className="p-4 font-sans text-lg font-bold text-center text-white rounded bg-hero">
+            React
+          </div>
+        </div>
+        <div className="grid gap-4 p-4 mt-4 lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1 ">
           <Portfolio />
-          <Portfolio />
+        </div>
+        <div className="p-4 mx-auto mt-4 ">
+          <div className="p-4 font-sans text-lg font-bold text-center text-white rounded bg-hero">
+            Woocommerce
+          </div>
         </div>
       </div>
     </>
