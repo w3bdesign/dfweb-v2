4725984a66c715976a30d3943ed4b2c77fcdff9a
w3bdesign <hijack_xxx@yahoo.com>
2020-04-16 03:08:20 +0200

    Add more boxes to portfolio

--- src/components/Animation/Portfolio.component.jsx
+++ src/components/Animation/Portfolio.component.jsx
@@ -1,42 +1,35 @@
 import * as React from "react"
-import { motion } from "framer-motion"
+//import { motion } from "framer-motion"
+
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 const container = {
-  hidden: { opacity: 1, scale: 0 },
+  hidden: { opacity: 0, scale: 0 },
   visible: {
     opacity: 1,
     scale: 1,
     transition: {
-      delay: 0.3,
+      delay: 1.0,
       when: "beforeChildren",
-      staggerChildren: 0.1,
+      staggerChildren: 1.0,
     },
-  },
-}
-
-const item = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
   },
 }
 
 export const Portfolio = () => (
   <>
-    
-    
-      {[0, 1, 2, 3].map((index) => (
-        <motion.div
-          key={index}
-          className="p-12 text-lg text-white rounded bg-hero"
-          variants={item}
-          initial="hidden"
-      animate="visible"
-        >
-          <p className="text-lg">Innhold Portefølje</p>
-        </motion.div>
-      ))}
-    
+    {[0, 1, 2, 3].map((index) => (
+      <motion.div
+        key={index}
+        className="p-12 text-lg text-white rounded bg-hero"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
+        <p className="m-2 text-lg">Innhold Portefølje</p>
+        <motion.div>Les mer innhold</motion.div>
+        
+      </motion.div>
+    ))}
   </>
 )
--- src/components/Portefølje/MainContent.js
+++ src/components/Portefølje/MainContent.js
@@ -1,19 +1,20 @@
 import React from "react"
 
-import {Portfolio} from "../Animation/Portfolio.component"
+import { Portfolio } from "../Animation/Portfolio.component"
 
 function MainContent() {
   return (
     <>
       <div className="container mx-auto rounded">
         <div className="container p-4 mx-auto mt-4 ">
-          <div className="p-12 font-sans text-lg text-white rounded bg-hero">            
-              Test
-             
+          <div className="p-12 font-sans text-lg text-white rounded bg-hero">
+            Test
           </div>
         </div>
         <div className="grid gap-4 p-4 mt-4 lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1 ">
-        <Portfolio/>
+          <Portfolio />
+          <Portfolio />
+          <Portfolio />
         </div>
       </div>
     </>
