38f4092fdd2c86112cba9938d9acfe5c2258d1da
w3bdesign <hijack_xxx@yahoo.com>
2020-04-17 04:45:42 +0200

    Update MainContent.component.jsx

--- src/components/NyIndex/MainContent.component.jsx
+++ src/components/NyIndex/MainContent.component.jsx
@@ -8,43 +8,52 @@ function MainContent() {
     <>
       <div className="mx-auto rounded shadow-large">
         <div className="mx-auto">
-          <div className="text-lg text-white rounded" style={{ backgroundImage: `url(${hero})`, height: "450px" }}>
-          
-           
-            <p className="text-3xl text-center">Hei! (Ny index)</p>
-            <p className="text-lg">
-              Jeg heter Daniel Fjeldstad og er en webutvikler som kan PHP,
-              mySQL, Wordpress, HTML, CSS, Javascript, React, Redux, Docker og
-              mye mer.
-            </p>
-            <FontAwesome />
+          <div
+            className="flex flex-col justify-center pr-16 text-lg text-white rounded"
+            style={{ backgroundImage: `url(${hero})`, height: "450px" }}
+          >
+            <div className="text-center ">
+              <p className="text-5xl">Hei!</p>
+              <p className="mt-4 text-3xl">
+                Velkommen til porteføljen til Daniel Fjeldstad
+              </p>
+            </div>
           </div>
         </div>
-        <div className="p-4 mt-4">
-          <div className="p-12 text-lg text-white rounded bg-hero">
-            <p className="text-3xl text-center">Om Meg</p>
-            <p className="mt-6 text-lg">
-              Siden 2000 har jeg arbeidet med HTML, CSS, PHP, mySQL og
-              Javascript. Jeg har arbeidet med Wordpress og Woocommerce siden
-              2011 hvor jeg har arbeidet frilans via Fiverr og kan skilte med
-              100% positiv tilbakemelding på samtlige oppdrag.
+        <div className="mt-4">
+          <div className="container max-w-4xl p-6 m-6 mx-auto text-lg text-left text-black bg-gray-100 rounded shadow-lg">
+            <p className="mt-6 text-3xl text-center">Om Meg</p>
+            <p className="mx-auto mt-8">
+              Jeg heter Daniel Fjeldstad og er en webutvikler som kan:
+              <br />
+              PHP, mySQL, Wordpress, HTML, CSS, Javascript, React, Redux, Docker
+              og mye mer.
             </p>
-            <p className="mt-6 text-lg">
+            <FontAwesome className="mt-8" />
+            <p className="mx-auto mt-8">
+              Siden 2000 har jeg arbeidet med HTML, CSS, PHP, mySQL og
+              Javascript.
+              <br />
+              Jeg har arbeidet med Wordpress og Woocommerce siden 2011 hvor jeg
+              har arbeidet frilans via Fiverr og kan skilte med 100% positiv
+              tilbakemelding på samtlige oppdrag.
+              <br />
               Har også erfaring med Opencart og Magento.
             </p>
-            <p className="mt-6 text-lg">
+
+            <p className="mx-auto mt-8">
               Jeg har også erfaring med andre teknologier som React, moderne
               Javascript (ES6+), Node.js, Firebase, Context, Redux, Axios,
               Docker med mer.
-              <p className="mt-6 text-lg">
-                På Portefølje kan du se eksempler på arbeid jeg har gjort i 2019
-                og 2020.
-              </p>
+              <br />
+              På Portefølje kan du se eksempler på arbeid jeg har gjort i 2019
+              og 2020.
             </p>
-            <p className="text-3xl text-center">Github</p>
-            <p className="mt-6 text-lg">
-              Jeg oppdaterer jevnlig min Github konto med nye prosjekter. Hvis
-              du er interessert kan du ta en titt{" "}
+            <p className="p-6 m-6 text-3xl text-center">Github</p>
+            <p className="mt-8">
+              Jeg oppdaterer jevnlig min Github konto med nye prosjekter.
+              <br />
+              Hvis du er interessert kan du ta en titt{" "}
               <a
                 className="underline"
                 href="https://github.com/w3bdesign"
