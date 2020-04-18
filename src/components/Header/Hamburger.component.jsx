683f43a53a7bf2d66ce11d4cf2b0b841a93be7f2
w3bdesign <hijack_xxx@yahoo.com>
2020-04-19 01:28:17 +0200

    Update Hamburger.component.jsx

--- src/components/Header/Hamburger.component.jsx
+++ src/components/Header/Hamburger.component.jsx
@@ -1,3 +1,7 @@
+// https://www.youtube.com/watch?v=97A3uRS7_YA
+// https://github.com/syntacticsolutions/react-navigation-menu
+// https://codesandbox.io/s/framer-motion-side-menu-mx2rw?fontsize=14&module=%2Fsrc%2FExample.tsx
+
 import React from "react"
 import HamburgerSVG from "../../svg/hamburger.svg"
 
