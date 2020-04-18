401adb6d5ecfe7c1106b8c0076ae6e4639848a38
w3bdesign <hijack_xxx@yahoo.com>
2020-04-18 04:38:43 +0200

    Update NavbarContent.component.jsx

--- src/components/Header/NavbarContent.component.jsx
+++ src/components/Header/NavbarContent.component.jsx
@@ -9,9 +9,8 @@ export default function NavbarContent() {
     <>
       <div className="container flex items-center mx-auto md:flex-wrap lg:flex-wrap xl:flex-wrap">
         <AppLogo />
-       
         <div className="flex content-center justify-between md:w-1/2 md:justify-end">
-        <Hamburger />
+          <Hamburger />
           <ul className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex">
             <li className="mr-3">
               <Link
