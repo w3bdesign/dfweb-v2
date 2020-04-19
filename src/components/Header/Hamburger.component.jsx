aa109ba5f1bceb07e43d940fd6db77bafe2f50bd
w3bdesign <hijack_xxx@yahoo.com>
2020-04-19 04:39:47 +0200

    Add hamburger hover

--- src/components/Header/Hamburger.component.jsx
+++ src/components/Header/Hamburger.component.jsx
@@ -2,13 +2,37 @@
 // https://github.com/syntacticsolutions/react-navigation-menu
 // https://codesandbox.io/s/framer-motion-side-menu-mx2rw?fontsize=14&module=%2Fsrc%2FExample.tsx
 
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "gatsby"
+
 import HamburgerSVG from "../../svg/hamburger.svg"
 
 export default function Hamburger() {
+  const [isExpanded, setisExpanded] = useState(false)
   return (
     <div className="md:hidden lg:hidden xl:hidden">
-      <HamburgerSVG className="w-10 m-4" style={{ height: "70px", color: "#fff" }} />
+      <HamburgerSVG
+        onClick={() => {
+          setisExpanded(!isExpanded)
+        }}
+        className="w-10 m-4 text-white hover:text-black"
+        style={{ height: "70px" }}
+      />
+      {isExpanded && (
+        <div className="mt-20 bg-white w-30 h-30">
+          <ul className="items-center justify-between flex-1 hidden list-reset md:flex lg:flex xl:flex">
+            <li className="mr-3">
+              <Link
+                className="inline-block m-4 text-xl text-white hover:underline"
+                activeClassName="underline"
+                to="/design3"
+              >
+                Hjem
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
