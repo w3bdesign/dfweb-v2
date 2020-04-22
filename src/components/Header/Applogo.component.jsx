bbac52851af3f85b0b223dde52d038129160ee46
w3bdesign <hijack_xxx@yahoo.com>
2020-04-22 19:02:58 +0200

    .

--- src/components/Header/Applogo.component.jsx
+++ src/components/Header/Applogo.component.jsx
@@ -1,13 +1,11 @@
 import React from "react"
 import Logo from "../../logo/logo.svg"
 
-// style={{ height: "70px" }}
-
 export default function AppLogo() {
   return (
     <div className="flex w-full font-extrabold text-white md:w-1/2 md:justify-start">
       <a className="text-white" href="/">
-        <Logo />
+        <Logo style={{ height: "70px" }}/>
       </a>
     </div>
   )
