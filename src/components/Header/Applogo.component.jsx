007ae1f462aaa15e97a12a1be5e7ec8ee6699340
w3bdesign <hijack_xxx@yahoo.com>
2020-04-22 18:58:41 +0200

    Gatsby testing

--- src/components/Header/Applogo.component.jsx
+++ src/components/Header/Applogo.component.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import Logo from "../../logo/logo.svg"
 
-
+// style={{ height: "70px" }}
 
 export default function AppLogo() {
   return (
     <div className="flex w-full font-extrabold text-white md:w-1/2 md:justify-start">
       <a className="text-white" href="/">
-        <Logo style={{ height: "70px" }} />
+        <Logo />
       </a>
     </div>
   )
