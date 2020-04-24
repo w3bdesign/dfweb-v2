d923283a0bcb2bdd860aff1f8cf0374771ccd480
w3bdesign <hijack_xxx@yahoo.com>
2020-04-24 02:07:34 +0200

    Create projects.js array
    We want a separate place to add new projects before we build our backend

---
+++ src/constants/PROJECTS.js
@@ -0,0 +1,28 @@
+export const PROJECTS = [
+  {
+    name: "Test Project",
+    description: "Test Description",
+    category: "React",
+    image: "",
+  },
+  {
+    name: "Test Project",
+    description: "Test Description",
+    category: "React",
+    image: "",
+  },
+  {
+    name: "Test Project",
+    description: "Test Description",
+    category: "React",
+    image: "",
+  },
+  {
+    name: "Test Project",
+    description: "Test Description",
+    category: "React",
+    image: "",
+  },
+]
+
+export default PROJECTS
