50ea827a9298ca698a3f166ae40c7a443342f9ed
w3bdesign <hijack_xxx@yahoo.com>
2020-05-06 18:39:59 +0200

    Remove Jest

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -3,6 +3,8 @@
 describe(`The Home Page`, () => {
   it(`successfully loads`, () => {
     cy.visit(`/`)
+   // cy.get('.nav').contains('About')
     //.waitForRouteChange()
   })
+ // cy.get('.nav').contains('About')
 })
