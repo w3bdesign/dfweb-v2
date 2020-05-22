af26c9cc87ff1966dc1d43e87e3068381fcb1809
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-22 21:16:14 +0200

    Add padding for mobile on frontpage

--- src/components/Portefølje/PorteføljeContent.component.jsx
+++ src/components/Portefølje/PorteføljeContent.component.jsx
@@ -52,24 +52,24 @@ function PorteføljeContent() {
     <>
       <main className="bg-graybg">
         <div className="container mx-auto rounded">
-          <div className="mx-auto mt-4">
+          <div className="px-4 mx-auto mt-4 lg:px-0 xl:px-0 md:px-0">
             <div className="p-4 text-2xl font-bold text-center text-black bg-white rounded shadow-lg">
               Javascript
             </div>
           </div>
-          <div className="grid gap-4 pt-4 pb-4 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
+          <div className="grid gap-4 px-4 pt-4 pb-4 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1">
             <Portfolio
               filter="Javascript"
               projects={projectdata.Project.allProjects}
             />
           </div>
-          <div className="mx-auto">
+          <div className="px-4 mx-auto lg:px-0 xl:px-0 md:px-0">
             <div className="p-4 font-sans text-2xl font-bold text-center text-black bg-white rounded shadow-lg">
               React
             </div>
           </div>
           <div
-            className="grid gap-4 pt-4 pb-4 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1"
+            className="grid gap-4 px-4 pt-4 pb-4 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1"
             ref={firstRevealContainer}
           >
             <Portfolio
@@ -77,13 +77,13 @@ function PorteføljeContent() {
               projects={projectdata.Project.allProjects}
             />
           </div>
-          <div className="mx-auto">
+          <div className="px-4 mx-auto lg:px-0 xl:px-0 md:px-0">
             <div className="p-4 font-sans text-2xl font-bold text-center text-black bg-white rounded shadow-lg">
               Woocommerce
             </div>
           </div>
           <div
-            className="grid gap-4 pt-4 pb-4 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1"
+           className="grid gap-4 px-4 pt-4 pb-4 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 xs:grid-cols-1"
             ref={secondRevealContainer}
           >
             <Portfolio
