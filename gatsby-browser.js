4d07a95e9258879fbcc8bbf9c63fef86091f7a96
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-29 04:28:30 +0200

    Add animate.min.css to SSR file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,7 @@ import "./src/css/main.css"
 import "./src/css/animate.min.css"
 
 import { config } from "@fortawesome/fontawesome-svg-core"
+
 // You should do that in a Layout file or in `gatsby-browser.js`.
 config.autoAddCss = false
+
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,4 @@
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import "./src/css/main.css"
+import "./src/css/animate.min.css"
+
