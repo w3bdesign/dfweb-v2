8fd3a2f9d640aecea13a72e53c57ac84d10a2700
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-07-15 19:36:17 +0200

    Npm update and decrease logo height a little

--- src/components/Header/Applogo.component.jsx
+++ src/components/Header/Applogo.component.jsx
@@ -9,7 +9,7 @@ export default function AppLogo() {
   return (
     <div className="flex w-full font-extrabold text-white md:w-1/2 md:justify-start">
       <a className="text-white" href="/">
-        <Logo className="h-12 lg:h-16 xl:h-16" />
+        <Logo className="h-12 p-2 lg:h-16 xl:h-16" />
       </a>
     </div>
   )
