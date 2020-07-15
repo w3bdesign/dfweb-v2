9888e2ddd350ecf200e337ea98bc17d005bafc4a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-07-15 19:49:33 +0200

    Decrease logo height

--- src/components/Index/MainContent.component.jsx
+++ src/components/Index/MainContent.component.jsx
@@ -14,7 +14,7 @@ function MainContent() {
   return (
     <>
       <main id="maincontent">
-        <div className="mx-auto mt-20 rounded bg-graybg shadow-large">
+        <div className="mx-auto mt-20 rounded bg-graybg shadow-large md:mt-16">
           <div
             id="main-hero"
             className="flex flex-col justify-center text-lg text-white animate__animated animate__fadeIn animate__delay-1s"
