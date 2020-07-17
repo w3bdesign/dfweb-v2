7be0e8eb5b90d63430384ab99368d76b67c371c8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-07-17 23:31:31 +0200

    We can now close the mobile menu by clicking outside of the menu

--- src/components/Header/Hamburger.component.jsx
+++ src/components/Header/Hamburger.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { Link } from "gatsby"
 
 import { HamburgerCollapse } from "react-animated-burgers"
@@ -12,14 +12,39 @@ import { HamburgerCollapse } from "react-animated-burgers"
 export default function Hamburger() {
   const [isExpanded, setisExpanded] = useState(false)
   const [isInitialRender, setisInitialRender] = useState(true)
+  const node = useRef()
+
+  const handleClickOutside = (e) => {
+    if (node.current.contains(e.target)) {
+      /**
+       * Clicked inside of the menu
+       */
+      return
+    }
+    /**
+     * Clicked outside of the menu
+     */
+    setisExpanded(false)
+  }
 
   const handleMobileMenuClick = () => {
     setisExpanded(!isExpanded)
     setisInitialRender(false)
   }
 
+  useEffect(() => {
+    if (isExpanded) {
+      document.addEventListener("mousedown", handleClickOutside)
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [isExpanded])
+
   return (
-    <div className="z-50 md:hidden lg:hidden xl:hidden">
+    <div ref={node} className="z-50 md:hidden lg:hidden xl:hidden">
       <HamburgerCollapse
         isActive={isExpanded}
         toggleButton={handleMobileMenuClick}
--- src/components/Header/Layout.component.jsx
+++ src/components/Header/Layout.component.jsx
@@ -15,9 +15,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-
       {children}
-
       <Footer />
     </>
   )
