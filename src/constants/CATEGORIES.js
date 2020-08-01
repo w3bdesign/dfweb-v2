63de8fa97bbad583589f71f3ba742119ce9ccc1c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-08-01 18:51:16 +0200

    Replace uuid with a unique ID in links
    Potential anti-pattern that may lead to worse performance

--- src/constants/CATEGORIES.js
+++ src/constants/CATEGORIES.js
@@ -1,14 +1,18 @@
 export const CATEGORIES = [
   {
+    id: 1,
     name: "React",
   },
   {
+    id: 2,
     name: "Javascript",
   },
   {
+    id: 3,
     name: "WooCommerce",
   },
   {
+    id: 4,
     name: "Magento",
   },
 ]
