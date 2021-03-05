86ac671f590a8fd4f3b87769263fca8d514af17d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-05 05:23:55 +0100

    Enable local testing with Testcafe

--- testcafe/index.js
+++ testcafe/index.js
@@ -1,7 +1,7 @@
  import { Selector } from 'testcafe';
 
 fixture `Check main page text`
-    .page `https://www.dfweb.no`;
+    .page `http://localhost:8000`;
 
 test('Verify text on front page', async t => {
     await t
