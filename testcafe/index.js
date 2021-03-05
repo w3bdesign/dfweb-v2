4d36e2cf2a4e0a236cee95f8fcf9701d65204538
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-05 05:52:46 +0100

    Finish implementing Testcafe in CircleCI

--- testcafe/index.js
+++ testcafe/index.js
@@ -1,18 +1,9 @@
- import { Selector } from 'testcafe';
+import { Selector } from "testcafe"
 
-fixture `Check main page text`
-    .page `http://localhost:8000`;
+fixture`Check main page text`.page`http://localhost:8000/`
 
-test('Verify text on front page', async t => {
-    await t
-        //.typeText('#developer-name', 'John Smith')
-        //.click('#submit-button')
-
-        // Use the assertion to check if the actual header text is equal to the expected one
-        
-        .expect(Selector('#daniel').innerText).eql('Jeg heter Daniel Fjeldstad og er en webutvikler.');
-        
-        // .debug()
-        // .expect(Selector('#article-header').innerText).eql('Will fail');
-
-});
+test("Verify text on front page", async (test) => {
+  await test
+    .expect(Selector("#daniel").innerText)
+    .eql("Jeg heter Daniel Fjeldstad og er en webutvikler.")
+})
