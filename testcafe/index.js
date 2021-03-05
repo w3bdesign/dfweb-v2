743434d8092530180c5801d00562048c86d4556a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-05 05:11:32 +0100

    Playing with Testcafe

--- testcafe/index.js
+++ testcafe/index.js
@@ -1,7 +1,7 @@
  import { Selector } from 'testcafe';
 
 fixture `Check main page text`
-    .page `/`;
+    .page `https://www.dfweb.no`;
 
 test('Verify text on front page', async t => {
     await t
