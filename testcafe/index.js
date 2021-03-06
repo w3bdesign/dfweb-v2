3ed08362160a4dd2b12e07ac0d425cba3bea1c96
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-06 23:29:07 +0100

    Add more testcafe tests

--- testcafe/index.js
+++ testcafe/index.js
@@ -1,22 +1,16 @@
 import { Selector } from "testcafe"
-// At the top of your test file
-import percySnapshot from '@percy/testcafe';
+import percySnapshot from "@percy/testcafe"
 
 // https://docs.percy.io/docs/testcafe
 // https://dev-tester.com/percy-and-testcafe-stop-letting-visual-bugs-slip-by-you/
 
-
 // eslint-disable-next-line no-unused-expressions
 fixture`Check main page text`.page`http://localhost:8000/`
 
-test("Verify text on front page", async (test) => {
+test("Verify text in Hero section", async (test) => {
   await test
-   // .expect(Selector("#daniel").innerText)
-   // .eql("Jeg heter Daniel Fjeldstad og er en webutvikler.")
-   .pressKey("enter")
-
-   await percySnapshot(test, "Created a todo");
-
-
-
+    .expect(Selector("#daniel").innerText)
+    .eql("Jeg heter Daniel Fjeldstad og er en webutvikler.")
+    .wait(2000)
+  await percySnapshot(test, "Loaded frontpage")
 })
---
+++ testcafe/prosjekter.js
@@ -0,0 +1,26 @@
+import { Selector } from "testcafe"
+import percySnapshot from "@percy/testcafe"
+
+const prosjektSelect = Selector("#kategorifilter")
+const prosjektOption = prosjektSelect.find("option")
+
+fixture`Check that category filter works`.page`http://localhost:8000/prosjekter`
+
+test(`Select Vue from the category filter and check that the value changes`, async (test) => {
+  await test
+    .click(prosjektSelect)
+    .click(prosjektOption.withText("Vue"))
+    .expect(prosjektSelect.value)
+    .eql("Vue")
+  await percySnapshot(test, "Clicked on Vue")
+})
+
+test(`Select Vue and check that the header content changes`, async (test) => {
+  await test
+    .click(prosjektSelect)
+    .click(prosjektOption.withText("Vue"))
+    .wait(2000)
+    .expect(Selector("#categoryFilterDiv").innerText)
+    .eql("Vue")
+    await percySnapshot(test, "Showing Vue projects")
+})
