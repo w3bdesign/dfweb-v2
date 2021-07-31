71bb7d308c3fcf762080baba452fb27304a19071
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-07-31 06:56:39 +0200

    .

--- src/components/pages/CV/CVContent.component.jsx
+++ src/components/pages/CV/CVContent.component.jsx
@@ -1,11 +1,10 @@
 import React from "react"
-// import { Document, Page, pdfjs } from "react-pdf"
-import { Document, Page } from "react-pdf"
+import { Document, Page, pdfjs } from "react-pdf"
 
 import Button from "../../layout/Button/Button.component"
 import myCV from "../../../assets/CV-dfweb.pdf"
 
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 /**
  * Displays the CV content using react-pdf
