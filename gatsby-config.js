a3260d42f66ae9685717ee377b4e4d3e75bfa1e4
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-15 23:57:14 +0100

    Uninstall Graphql source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,7 @@ module.exports = {
     twitterUsername: "@occlumency",
   },
   plugins: [
-    "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // This type will contain the remote schema Query type
-        typeName: "Project",
-        // This is the field under which it's accessible
-        fieldName: "Project",
-        // URL to query from
-        url: "https://graphql-express-dfweb.vercel.app",
-      },
-    },
+    "gatsby-plugin-postcss",    
     {
       resolve: "gatsby-plugin-manifest",
       options: {
