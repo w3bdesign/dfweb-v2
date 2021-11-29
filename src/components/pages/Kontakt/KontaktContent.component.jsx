1da317c7211d955f8f7b55bb891fe66e9f7f458e
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-29 01:50:25 +0100

    Readd Kontakt

--- src/components/pages/Kontakt/KontaktContent.component.jsx
+++ src/components/pages/Kontakt/KontaktContent.component.jsx
@@ -1,81 +1,106 @@
-import React from "react"
+import React, { useState, useRef } from "react"
+import { init, sendForm } from "emailjs-com"
 
 import Button from "../../layout/Button/Button.component"
 
-export default function KontaktContent() {
+function KontaktContent() {
+  const form = useRef()
+  const [serverResponse, setServerResponse] = useState("")
+
+  const handleSubmit = (event) => {
+    const EMAIL_API_KEY = process.env.GATSBY_EMAIL_API_KEY
+    console.log("ID: ", EMAIL_API_KEY)
+    event.preventDefault()
+    init(EMAIL_API_KEY) //TODO Save this as an environment variable
+    sendForm("service_j9zgvxu", "template_d6ntuci", form.current).then(
+      () => {
+        setServerResponse("Takk for din beskjed")
+      },
+      () => {
+        setServerResponse("Feil under sending av skjema")
+      }
+    )
+  }
   return (
-    <>
-      <main id="maincontent">
-        <div className="mt-24 bg-graybg">
-          <div className="px-4 p lg:px-0 xl:px-0 md:px-0">
-            <div className="container mx-auto bg-white rounded shadow">
-              <div className="p-4 mx-auto mt-4">
-                <div className="p-4 text-lg rounded">
-                  <h3 className="m-2 text-3xl text-center text-black">
-                    Kontakt
-                  </h3>
-                  <form
-                    className="text-center"
-                    name="Kontakt"
-                    method="POST"
-                    data-netlify="true"
-                  >
-                    <input type="hidden" name="form-name" value="Kontakt" />
-                    <fieldset>
-                      <legend className="container m-4 mx-auto text-xl text-center">
-                        Vennligst fyll ut kontaktskjemaet:{" "}
-                      </legend>
-                      <label htmlFor="navn" className="text-black">
-                        Fullt navn
+    <main id="maincontent">
+      <div className="mt-24 bg-graybg">
+        <div className="px-4 p lg:px-0 xl:px-0 md:px-0">
+          <div className="container mx-auto bg-white rounded shadow">
+            <div className="p-4 mx-auto mt-4">
+              <div className="p-4 text-lg rounded">
+                <h3 className="m-12 text-3xl text-center text-green">
+                  {serverResponse}
+                </h3>
+                {!serverResponse && (
+                  <>
+                    <h3 className="m-2 text-3xl text-center text-black">
+                      Kontakt
+                    </h3>
+                    <form
+                      className="text-center"
+                      ref={form}
+                      onSubmit={handleSubmit}
+                      method="POST"
+                      action="/api/form"
+                    >
+                      <fieldset>
+                        <legend className="container m-4 mx-auto text-xl text-center">
+                          Vennligst fyll ut kontaktskjemaet:{" "}
+                        </legend>
+                        <label htmlFor="navn" className="text-black">
+                          Fullt navn
+                          <br />
+                          <input
+                            className="w-64 p-2 m-2 placeholder-black transition duration-500 ease-in-out border border-gray-500 rounded focus:shadow-outline focus:scale-x-110 hover:bg-gray-200 transform-gpu"
+                            id="navn"
+                            name="navn"
+                            type="text"
+                            required
+                            aria-required
+                          />
+                        </label>
                         <br />
-                        <input
-                          className="w-64 p-2 m-2 placeholder-black transition duration-500 ease-in-out border border-gray-500 rounded focus:shadow-outline focus:scale-x-110 hover:bg-gray-200 transform-gpu"
-                          id="navn"
-                          name="navn"
-                          type="text"
-                          required
-                          aria-required
-                        />
-                      </label>
-                      <br />
-                      <label className="text-black" htmlFor="phone">
-                        Telefonnummer (i norskt format)
+                        <label className="text-black" htmlFor="phone">
+                          Telefonnummer (i norskt format)
+                          <br />
+                          <input
+                            className="w-64 p-2 m-2 placeholder-black transition duration-500 ease-in-out border border-gray-500 rounded focus:scale-x-110 focus:shadow-outline hover:bg-gray-200 transform-gpu"
+                            id="phone"
+                            name="telefon"
+                            type="text"
+                            required
+                            aria-required
+                            pattern=".[0-9]{7}"
+                          />
+                        </label>
                         <br />
-                        <input
-                          className="w-64 p-2 m-2 placeholder-black transition duration-500 ease-in-out border border-gray-500 rounded focus:scale-x-110 focus:shadow-outline hover:bg-gray-200 transform-gpu"
-                          id="phone"
-                          name="telefon"
-                          type="text"
-                          required
-                          aria-required
-                          pattern=".[0-9]{7}"
-                        />
-                      </label>
-                      <br />
-                      <label className="text-black" htmlFor="textarea">
-                        Hva ønsker du å si?
-                        <br />
-                        <textarea
-                          className="w-64 p-2 m-2 placeholder-black transition duration-500 ease-in-out border border-gray-500 rounded focus:shadow-outline focus:scale-x-110 hover:bg-gray-200 transform-gpu"
-                          name="tekst"
-                          id="textarea"
-                          required
-                          aria-required
-                        />
-                      </label>
-                    </fieldset>
-                    <Button>Send skjema</Button>
-                  </form>
-                </div>
+                        <label className="text-black" htmlFor="textarea">
+                          Hva ønsker du å si?
+                          <br />
+                          <textarea
+                            className="w-64 p-2 m-2 placeholder-black transition duration-500 ease-in-out border border-gray-500 rounded focus:shadow-outline focus:scale-x-110 hover:bg-gray-200 transform-gpu"
+                            name="tekst"
+                            id="textarea"
+                            required
+                            aria-required
+                          />
+                        </label>
+                      </fieldset>
+                      <Button>Send skjema</Button>
+                    </form>
+                  </>
+                )}
               </div>
             </div>
           </div>
-          <div id="kontakt-filler" className="mt-48">
-            <br />
-            <br />
-          </div>
         </div>
-      </main>
-    </>
+        <div id="kontakt-filler" className="mt-48">
+          <br />
+          <br />
+        </div>
+      </div>
+    </main>
   )
 }
+
+export default KontaktContent
