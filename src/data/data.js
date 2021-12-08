a89321bd222ea53b7f7a23cd4ed7af305b1ab581
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2021-12-08 02:47:07 +0100

    Replace live url for Dfweb

--- src/data/data.js
+++ src/data/data.js
@@ -6,7 +6,7 @@ exports.data = [
       "Nettbutikk med WooCommerce som backend. Produktene hentes via en GraphQL endpoint via WPGraphQL WordPress plugin og benytter seg av Tailwind for design. Produktsøk via Algolia-integrasjon. Formvalidering gjøres via React Hook Form.",
     subdescription: "NextJS, React, GraphQL, Tailwind, React Hook Form med mer",
     category: "React",
-    urlwww: "https://nextjs-woocommerce.now.sh/",
+    urlwww: "https://nextjs-woocommerce.dfweb.no",
     urlgithub: "https://github.com/w3bdesign/nextjs-woocommerce",
     image:
       "https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1590539031/Dfweb/nextjs-woocommerce_kknqkg.jpg",
@@ -43,7 +43,7 @@ exports.data = [
       "Porteføljen er lagd med Gatsby og React. Animasjoner er implementert via Animate.css og React-Spring. For design har jeg brukt Tailwind.",
     subdescription: "Gatsby, React-Spring, Animate.css, Tailwind, Cypress",
     category: "React",
-    urlwww: "https://www.dfweb.no/",
+    urlwww: "https://www.dfweb.no",
     urlgithub: "https://github.com/w3bdesign/dfweb",
     image:
       "https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588559314/Dfweb/dfweb-v2_g9wjyw.jpg",
@@ -55,8 +55,8 @@ exports.data = [
       "Plugin for å sende WooCommerce-produkter til Algolia. Pluginen er kodet i PHP 7 og inkluderer oversettelser via .PO og .MO filer. Koden er testet i PHPCS. ",
     subdescription: "WordPress, PHP, OOP, PHPCS, POT",
     category: "PHP",
-    urlwww: "https://github.com/w3bdesign/algolia-woo-indexer/",
-    urlgithub: "https://github.com/w3bdesign/algolia-woo-indexer/",
+    urlwww: "https://github.com/w3bdesign/algolia-woo-indexer",
+    urlgithub: "https://github.com/w3bdesign/algolia-woo-indexer",
     image:
       "https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1594424771/Dfweb/algolia-woocommerce-indexer.jpg",
   },
@@ -68,7 +68,7 @@ exports.data = [
     subdescription:
       "React, React-Testing-Library, Cypress, Easy Peasy, React Bootstrap med mer",
     category: "React",
-    urlwww: "https://steinsakspapir.dfweb.no/",
+    urlwww: "https://steinsakspapir.dfweb.no",
     urlgithub: "https://github.com/w3bdesign/stein-saks-papir",
     image:
       "https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1601217905/Dfweb/stein-saks-papir_w9joyf.jpg",
@@ -80,7 +80,7 @@ exports.data = [
       "Henter informasjon fra Statens Vegvesen sitt REST API med en serverless backend som benyttes som proxy. Bruker Parcel bundler og er kodet i Typescript med HTML5 og Materialize for design.",
     subdescription: "HTML5, Typescript, OOP, Materialize, Parcel, Serverless",
     category: "Typescript",
-    urlwww: "https://vegvesen.dfweb.no/",
+    urlwww: "https://vegvesen.dfweb.no",
     urlgithub: "https://github.com/w3bdesign/Statens-Vegvesen",
     image:
       "https://res.cloudinary.com/duelisue0/image/upload/c_fill,h_255,w_500/v1588556573/Dfweb/statens-vegvesen_xsth2e.jpg",
