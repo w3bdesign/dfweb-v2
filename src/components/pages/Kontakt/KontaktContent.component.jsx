c04cee63784fcefb721b18ad365edbd06b4c17f9
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2022-01-02 05:54:16 +0100

    Update KontaktContent.component.jsx

--- src/components/pages/Kontakt/KontaktContent.component.jsx
+++ src/components/pages/Kontakt/KontaktContent.component.jsx
@@ -71,7 +71,7 @@ function KontaktContent() {
                       action="/api/form"
                     >
                       <fieldset>
-                        <legend className="container m-4 mx-auto text-xl text-center w-32">
+                        <legend className="container m-4 mx-auto text-xl text-center w-64">
                           Vennligst fyll ut kontaktskjemaet:{" "}
                         </legend>
                         <label htmlFor="navn" className="text-black">
