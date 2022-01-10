50654cb24265220c5a99f7c88074de09c1df5756
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-10 19:00:02 +0100

    Bad value https://www.w3.org/2000/svg for the attribute xmlns (only http://www.w3.org/2000/svg permitted here)

--- src/components/layout/SVGIcons/SVG/FontAwesomeCopyright.jsx
+++ src/components/layout/SVGIcons/SVG/FontAwesomeCopyright.jsx
@@ -9,7 +9,7 @@ function FontAwesomeCopyright() {
       data-prefix="far"
       data-icon="copyright"
       role="img"
-      xmlns="https://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
     >
       <path
