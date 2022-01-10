341471a700e57eab41f776a15625ddbf5ca56abb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-10 21:29:53 +0100

    Revert "Test CSP headers"
    This reverts commit d777e6936cb38a2f38495c49e0cfa9803220bfc6.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,1 @@
-import React from 'react'
-import Helmet from "react-helmet"
-
 import "./src/css/main.css"
-
-export const wrapPageElement = ({ element }) => (
-  <>
-    <Helmet>
-      <meta
-        http-equiv="Content-Security-Policy"
-        content="default-src 'self'; img-src https://*; child-src 'none';"
-      />
-    </Helmet>
-
-    {element}
-  </>
-)
