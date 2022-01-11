5c2707d67b62259bd72ea86a3da1b120e4cf14de
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-11 01:52:11 +0100

    Gjør hamburger knapp bare for de uten screen reader

--- src/components/layout/Header/Hamburger.component.jsx
+++ src/components/layout/Header/Hamburger.component.jsx
@@ -14,7 +14,7 @@ function Hamburger() {
   const [isExpanded, setisExpanded] = useState(false)
   const [isInitialRender, setisInitialRender] = useState(true)
   const hamburgerLine =
-    "h-1 w-10 my-1 rounded-full bg-white transition ease transform duration-300"
+    "h-1 w-10 my-1 rounded-full bg-white transition ease transform duration-300 not-sr-only"
 
   const fadeInUpCSS = classNames(
     "absolute right-0 w-full text-center bg-gray-800 mt-4 w-30",
